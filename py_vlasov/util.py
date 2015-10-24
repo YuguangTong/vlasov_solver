add a couple of unittest. find a bug in calculation of plasma dispersion function. scipy.special.dawnsn does not perform as expected for input such as (0.013 + 4.9j). switch to scipy.special.wofz, the cpp implementation of faddeeva function. need to continue with unittest

py_vlasov/util.py
@@ -53,14 +53,14 @@ def pade(z):
 def zp(z):
     """
     Plasma dispersion function
-    Utilize the Dawnson function, dawsn, in scipy.special module.
+    Utilize the faddeeva function, wofz, in scipy.special module.
 
     Keyword arguments:
     z: dimensionless argument of the plasma dispersion function.
     
     Return the value of Zp(z)
     """
-    return -2. * scipy.special.dawsn(z) + 1.j * np.sqrt(np.pi) * np.exp(- z **2)
+    return 1.j * np.sqrt(np.pi) * scipy.special.wofz(z)
     
 def zpd(x):
     """