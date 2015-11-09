find out the bug that leads to wrong result in presence of temperature anisotropy. pass tests to search over anisotropy.

py_vlasov/dispersion_tensor.py
@@ -23,7 +23,7 @@ def f_zeta(w, kz, vz, Omega, vthz, n):
     """
     return (w-kz*vz-n*Omega)/(kz*vthz)
 
-def f_abn(n, w, kz, tp, tz, vthz, Omega, vz, method='pade'):
+def f_abn(n, w, kz, tz, tp, vthz, Omega, vz, method='pade'):
     """
     Calculate the coefficient \A_n and \B_n, as defined in Stix(1992).
     
@@ -77,7 +77,7 @@ def f_lambda(kp, vthp, Omega):
     """
     return (kp * vthp/Omega)**2 / 2
 
-def f_yn(n, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method = 'pade'):
+def f_yn(n, w, kz, kp, tz, tp, vthz, vthp, Omega, vz, method = 'pade'):
     """
     Calculate tensor Y_n as defined in 'Waves in plamas' (p 258, Stix 1992)
     
@@ -99,10 +99,10 @@ def f_yn(n, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method = 'pade'):
     return tensor Y_n (times w)
     """
     
-    lamb = f_lambda(kp, vthz, Omega)
+    lamb = f_lambda(kp, vthp, Omega)
     i_n = scipy.special.iv(n, lamb)
     i_np = 0.5 * (scipy.special.iv(n-1, lamb) + scipy.special.iv(n+1, lamb))
-    an, bn = f_abn(n, w, kz, tp, tz, vthz, Omega, vz, method)
+    an, bn = f_abn(n, w, kz, tz, tp, vthz, Omega, vz, method)
     y = np.zeros((3, 3), dtype = np.cfloat)
     y[0, 0] = n**2 * i_n/lamb * an
     y[0, 1] = -1j * n * (i_n - i_np) * an
@@ -139,12 +139,12 @@ def f_chi(n, w, kz, kp, wp, tz, tp, vthz, vthp, Omega, vz, method = 'pade'):
     """
     chi_tensor = np.zeros((3, 3), dtype = np.cfloat)
     chi_tensor[2, 2] = 2 * wp**2 * w/ (kz * vthp**2) * vz
-    lamb = f_lambda(kp, vthz, Omega)
+    lamb = f_lambda(kp, vthp, Omega)
     # y_sum = np.sum(np.array([f_yn(i, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method) \
     #                          +f_yn(-i, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method) \
     #                          for i in np.arange(1, n+1)]), axis=0)
     # y_sum += f_yn(0, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method)
-    y_sum = np.sum(np.array([f_yn(i, w, kz, kp, tp, tz, vthz, vthp, Omega, vz, method) for i in np.arange(-n, n+1)]), axis=0)
+    y_sum = np.sum(np.array([f_yn(i, w, kz, kp, tz, tp, vthz, vthp, Omega, vz, method) for i in np.arange(-n, n+1)]), axis=0)
     chi_tensor += wp**2 * np.exp(-lamb) * y_sum
     return chi_tensor
 

py_vlasov/new_follow_parameter.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # consider expansion to include changes in mass
 #---------------------------------------------#
 def follow_eigen_freq(seed_freq, target_param, target_value,
-                      param, num, guess_fn, show_plot=False):
+                      param, num, show_plot=False):
     """
     Follow the frequency of an eigenmode of a plasma along a
     parameter ('target_param') till the parameter reach a
@@ -30,29 +30,21 @@ def follow_eigen_freq(seed_freq, target_param, target_value,
     show_plot: whether make a plot for the frequencies stepped through
     """
     if target_param == 'kz':
-        return follow_kz(seed_freq, target_value,
-                         param, num, guess_fn, show_plot=False)
+        return follow_kz(seed_freq, target_value,param, num)
     elif target_parm == 'kp':
-        return follow_kp(seed_freq, target_param, target_value,
-                         param, num, guess_fn, show_plot=False)
+        return follow_kp(seed_freq, target_param, target_value, param, num)
     elif target_parm == 'k':
-        return follow_k(seed_freq, target_value,
-                        param, num, guess_fn, show_plot=False)
+        return follow_k(seed_freq, target_value, param, num)
     elif target_parm == 'angle':
-        return follow_angle(seed_freq, target_value,
-                            param, num, guess_fn, show_plot=False)
+        return follow_angle(seed_freq, target_value, param, num)
     elif target_parm == 'beta':
-        return follow_beta(seed_freq, target_value,
-                           param, num, guess_fn, show_plot=False)
+        return follow_beta(seed_freq, target_value, param, num)
     elif target_parm == 'temperature':
-        return follow_temperature(seed_freq, target_value,
-                                  param, num, guess_fn, show_plot=False)
+        return follow_temperature(seed_freq, target_value, param, num)
     elif target_parm == 'anisotropy':
-        return follow_anisotropy(seed_freq, target_value,
-                                 param, num, guess_fn, show_plot=False)
+        return follow_anisotropy(seed_freq, target_value, param, num)
     elif target_parm == 'drift':
-        return follow_drift(seed_freq, target_value,
-                            param, num, guess_fn, show_plot=False)
+        return follow_drift(seed_freq, target_value, param, num)
     else:
         raise VlasovException('{0} is not a valid parameter to vary'.
                               format(target_param))
@@ -137,16 +129,15 @@ def generate_steps(start, end, log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = '
         return generate_1d_steps(start, end, log_incrmt,
                                  lin_incrmt, incrmt_method)
     else:
-        print('start', start)
-        print('end', end)
         assert hasattr(start, '__iter__') and hasattr(end, '__iter__')
         return generate_2d_steps(start, end, log_incrmt,
                                  lin_incrmt, incrmt_method)
     
-def follow_kz(seed_freq, target_value, param, guess_fn, show_plot=False,
+def follow_kz(seed_freq, target_value, param, show_plot=False,
               log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
     """
-    to implement
+    follow a mode with frequency SEED_FREQ in a plasma specified by
+    PARAM along the kz parameter.
     """
     (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
      v_list, n, method, aol) = param
@@ -168,17 +159,42 @@ def follow_kz(seed_freq, target_value, param, guess_fn, show_plot=False,
         plt.plot(kz_list, np.real(freq_lst), 'o-', markersize= 2)
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlabel(r'$k\rho_p$')
+        plt.xlabel(r'$kz\rho_p$')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
-        plt.title(r'Change $k\rho_p$ from {0} to {1}'.format(seed_kz, target_value))
+        plt.title(r'Change $kz\rho_p$ from {0} to {1}'.format(seed_kz, target_value))
         plt.show()        
     return guess
 
-def follow_kp(seed_freq, target_value, param, increment,guess_fn, show_plot=False):
+def follow_kp(seed_freq, target_value, param, show_plot=False,
+              log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
     """
     to implement
     """
-    return 0
+    (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+     v_list, n, method, aol) = param
+    seed_kp = kp
+    # a list of kp to step through
+    kp_list = generate_steps(kp, target_value, log_incrmt=log_incrmt,
+                             lin_incrmt=lin_incrmt, incrmt_method = incrmt_method)
+
+    freq_lst = []
+    guess = seed_freq
+    for kp in kp_list:
+        f = lambda wrel: real_imag(disp_det(
+            list_to_complex(wrel), kz, kp, beta, t_list, a_list,
+            n_list, q_list, m_list, v_list, n=n, method=method, aol=aol))
+        freq = scipy.optimize.fsolve(f, real_imag(guess))
+        guess = list_to_complex(freq)
+        freq_lst += [guess]
+    if show_plot:
+        plt.plot(kp_list, np.real(freq_lst), 'o-', markersize= 2)
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel(r'$kp\rho_p$')
+        plt.ylabel(r'$\omega/\Omega_{ci}$')
+        plt.title(r'Change $kp\rho_p$ from {0} to {1}'.format(seed_kp, target_value))
+        plt.show()        
+    return guess
 
 def follow_angle(seed_freq, target_value, param, increment,guess_fn, show_plot=False):
     """
@@ -205,11 +221,38 @@ def follow_temperature(seed_freq, target_value, param, increment,guess_fn, show_
     """    
     return 0
 
-def follow_anisotropy(seed_freq, target_value, param, increment,guess_fn, show_plot=False):
+def follow_anisotropy(seed_freq, target_value, param, show_plot=False,
+              log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
     """
-    to implement
-    """    
-    return 0
+    follow a mode with frequency SEED_FREQ in a plasma specified by
+    PARAM along the anisotropy parameter.
+    """
+    (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+     v_list, n, method, aol) = param
+    seed_a_list = a_list
+    # a list of kp to step through
+    a_list_steps = generate_steps(a_list, target_value, log_incrmt=log_incrmt,
+                             lin_incrmt=lin_incrmt, incrmt_method = incrmt_method)
+
+    freq_lst = []
+    guess = seed_freq
+    for a_list in a_list_steps:
+        f = lambda wrel: real_imag(disp_det(
+            list_to_complex(wrel), kz, kp, beta, t_list, a_list,
+            n_list, q_list, m_list, v_list, n=n, method=method, aol=aol))
+        freq = scipy.optimize.fsolve(f, real_imag(guess))
+        guess = list_to_complex(freq)
+        freq_lst += [guess]
+    if show_plot:
+        plt.plot(a_list_steps[:,0], np.real(freq_lst), 'o-', markersize= 2)
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel(r'$T_{p\perp}/T_{p\parallel}$')
+        plt.ylabel(r'$\omega/\Omega_{ci}$')
+        plt.title(r'Change $T_{s\perp}/T_{s\parallel}$')
+        plt.show()        
+    return guess
+   
 
 def follow_drift(seed_freq, target_value, param, increment, guess_fn, show_plot=False):
     """

unittest/test_dispersion_tensor.py
@@ -20,7 +20,7 @@ class Test_dispersion_tensor(unittest.TestCase):
         omega = 0.34
         vz = 3.4e3
         
-        an, bn = f_abn(n, w, kz, tp, tz, vthz, omega, vz, method='numpy')
+        an, bn = f_abn(n, w, kz, tz, tp, vthz, omega, vz, method='numpy')
         an, bn = an/w, bn/w
         npt.assert_allclose(an, -1.9218850625427564 + 5.595463531399411j,
                             rtol=1e-7)
@@ -28,7 +28,7 @@ class Test_dispersion_tensor(unittest.TestCase):
                             rtol=1e-7)
 
         w = 0.03
-        an, bn = f_abn(n, w, kz, tp, tz, vthz, omega, vz, method='numpy')
+        an, bn = f_abn(n, w, kz, tz, tp, vthz, omega, vz, method='numpy')
         an, bn = an/w, bn/w
         npt.assert_allclose(an, 631.2715006824648 - 46.41868239430783j,
                             rtol=1e-7)
@@ -36,7 +36,7 @@ class Test_dispersion_tensor(unittest.TestCase):
                             rtol=1e-7)
         
         n, w = 5, 0.03
-        an, bn = f_abn(n, w, kz, tp, tz, vthz, omega, vz, method='numpy')
+        an, bn = f_abn(n, w, kz, tz, tp, vthz, omega, vz, method='numpy')
         an, bn = an/w, bn/w
         npt.assert_allclose(bn, 1.6137363813074678e6 + 2.3743306625716157e7j,
                             rtol=1e-7)
@@ -44,7 +44,7 @@ class Test_dispersion_tensor(unittest.TestCase):
                             rtol=1e-7)
 
         n, w, kz, vthz = 20, 3.7, 0.11, 0.35
-        an, bn = f_abn(n, w, kz, tp, tz, vthz, omega, vz, method='numpy')
+        an, bn = f_abn(n, w, kz, tz, tp, vthz, omega, vz, method='numpy')
         an, bn = an/w, bn/w
         npt.assert_allclose(an, -0.26539669257210097 -0.0j, \
                             rtol=1e-7)
@@ -67,7 +67,7 @@ class Test_dispersion_tensor(unittest.TestCase):
         omega = 0.1
         vz = 100.
 
-        yn = f_yn(n, w, kz, kp, tp, tz, vthz, vthp, omega, vz, method = 'numpy')/w
+        yn = f_yn(n, w, kz, kp, tz, tp, vthz, vthp, omega, vz, method = 'numpy')/w
         expected_yn = np.array(
             [[0,0,0],
              [0, -1.072552723 + 0.6510730508j, -6.510730508 - 0.7604398751j],
@@ -122,5 +122,29 @@ class Test_dispersion_tensor(unittest.TestCase):
             [-3.086420946e8, 3.661518437e9+3.465700712e8j, -1.976136342e9 + 2.497184730e10j]])
         npt.assert_allclose(chi, expected_chi, rtol = 1e-7)
 
+    def test_chi_3(self):
+        """
+        test chi tensor as defined in Stix (1992).
+        """
+        NN = 3
+        w = 0.09578833596200093
+        kz = 1.5975774807850725e-06
+        kp = 1.5975774807850725e-06
+        wp = 4789.4167982678364
+        tz = 217763.9218377612
+        tp = 195987.52965398508
+        vthz = 59958.491600000001
+        vthp = 56881.619557222031
+        omega = 0.9578833596200093
+        vz = 0.
+
+        chi = f_chi(NN, w, kz, kp, wp, tz, tp, vthz, vthp,
+                    omega, vz, method = 'numpy')
+        expected_chi = np.array([[246532.0684, 2.3160720168e6j, -14138.594698],
+                                 [-2.31607201685e6j, 25210.8159178 + 
+                                  120337.3246248j,-1.33708138e6-143053.010866j],
+                                 [-14138.594698, 1.33708138472e6 + 143053.010866j, -3.4654579662e6 + 2.977992432056e7j]])
+        npt.assert_allclose(chi, expected_chi, rtol = 1e-7)
+        
 if __name__ == '__main__':
     unittest.main()