1f0002302ba8d3bf062fc1833a1fa40a77d3aaf0
Yuguang Tong <ygtong@berkeley.edu>
2015-12-04 14:49:44 -0800

    add the whole list of frequencies to the output of scan functions along a parameter

--- py_vlasov/new_follow_parameter.py
+++ py_vlasov/new_follow_parameter.py
@@ -156,14 +156,16 @@ def follow_kz(seed_freq, target_value, param, show_plot=False,
         guess = list_to_complex(freq)
         freq_lst += [guess]
     if show_plot:
-        plt.plot(kz_list, np.real(freq_lst), 'o-', markersize= 2)
+        plt.plot(kz_list, np.abs(np.real(freq_lst)), 'o-', markersize= 2)
         plt.xscale('log')
         plt.yscale('log')
         plt.xlabel(r'$kz\rho_p$')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
         plt.title(r'Change $kz\rho_p$ from {0} to {1}'.format(seed_kz, target_value))
-        plt.show()        
-    return guess
+        plt.show()
+    new_param = (target_value, kp, beta, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
 def follow_kp(seed_freq, target_value, param, show_plot=False,
               log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
@@ -187,14 +189,16 @@ def follow_kp(seed_freq, target_value, param, show_plot=False,
         guess = list_to_complex(freq)
         freq_lst += [guess]
     if show_plot:
-        plt.plot(kp_list, np.real(freq_lst), 'o-', markersize= 2)
+        plt.plot(kp_list, np.abs(np.real(freq_lst)), 'o-', markersize= 2)
         plt.xscale('log')
         plt.yscale('log')
         plt.xlabel(r'$kp\rho_p$')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
         plt.title(r'Change $kp\rho_p$ from {0} to {1}'.format(seed_kp, target_value))
         plt.show()        
-    return guess
+    new_param = (kz, target_value, beta, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
 def follow_angle(seed_freq, target_value, param, increment,guess_fn, show_plot=False):
     """
@@ -203,10 +207,39 @@ def follow_angle(seed_freq, target_value, param, increment,guess_fn, show_plot=F
     
     return 0
 
-def follow_k(seed_freq, target_value, param, increment,guess_fn, show_plot=False):
+def follow_k(seed_freq, target_value, param, show_plot=False,
+             log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
     """
     """    
-    return 0
+    (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+     v_list, n, method, aol) = param
+    seed_k = np.sqrt(kz**2 + kp**2)
+    kz_k = kz/seed_k
+    kp_k = kp/seed_k
+    # a list of k to step through
+    k_list = generate_steps(seed_k, target_value, log_incrmt=log_incrmt,
+                             lin_incrmt=lin_incrmt, incrmt_method = incrmt_method)
+    freq_lst = []
+    guess = seed_freq
+    for k in k_list:
+        kz, kp = k * kz_k, k * kp_k
+        f = lambda wrel: real_imag(disp_det(
+            list_to_complex(wrel), kz, kp, beta, t_list, a_list,
+            n_list, q_list, m_list, v_list, n=n, method=method, aol=aol))
+        freq = scipy.optimize.fsolve(f, real_imag(guess))
+        guess = list_to_complex(freq)
+        freq_lst += [guess]
+    if show_plot:
+        plt.plot(k_list, np.abs(np.real(freq_lst)), 'o-', markersize= 2)
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel(r'$k\rho_p$')
+        plt.ylabel(r'$\omega/\Omega_{ci}$')
+        plt.title(r'Change $k\rho_p$ from {0} to {1}'.format(seed_k, target_value))
+        plt.show()        
+    new_param = (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
 def follow_beta(seed_freq, target_value, param, show_plot=False,
                 log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
@@ -231,15 +264,17 @@ def follow_beta(seed_freq, target_value, param, show_plot=False,
         guess = list_to_complex(freq)
         freq_lst += [guess]
     if show_plot:
-        plt.plot(beta_list, np.real(freq_lst), 'o-', markersize= 2)
+        plt.plot(beta_list, np.abs(np.real(freq_lst)), 'o-', markersize= 2)
         plt.xscale('log')
         plt.yscale('log')
         plt.xlabel(r'$\beta_p$')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
         plt.title(r'Change $\beta_p$ from {0} to {1}'.
-                  format(beta_seed, target_value))
+                  format(seed_beta, target_value))
         plt.show()        
-    return guess
+    new_param = (kz, kp, target_value, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
 
 def follow_temperature(seed_freq, target_value, param, show_plot=False,
@@ -265,13 +300,14 @@ def follow_temperature(seed_freq, target_value, param, show_plot=False,
         guess = list_to_complex(freq)
         freq_lst += [guess]
     if show_plot:
-        plt.plot(t_list_steps[:,0], np.real(freq_lst), 'o-', markersize= 2)
-        plt.xscale('log')
+        plt.plot(np.abs(np.real(freq_lst)),'o-', markersize= 2)
         plt.yscale('log')
-        plt.xlabel(r'$T_{s\parallel}/T_{p\parallel}$')
+        plt.xlabel('step')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
         plt.show()        
-    return guess
+    new_param = (kz, kp, beta, target_value, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
 def follow_anisotropy(seed_freq, target_value, param, show_plot=False,
               log_incrmt=0.1, lin_incrmt=0.1, incrmt_method = 'log'):
@@ -296,21 +332,102 @@ def follow_anisotropy(seed_freq, target_value, param, show_plot=False,
         guess = list_to_complex(freq)
         freq_lst += [guess]
     if show_plot:
-        plt.plot(a_list_steps[:,0], np.real(freq_lst), 'o-', markersize= 2)
+        plt.plot(a_list_steps[:,0], np.abs(np.real(freq_lst)),
+                 'o-', markersize= 2)
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlabel(r'$T_{p\perp}/T_{p\parallel}$')
+        plt.xlabel('step')
         plt.ylabel(r'$\omega/\Omega_{ci}$')
         plt.title(r'Change $T_{s\perp}/T_{s\parallel}$')
         plt.show()        
-    return guess
+    new_param = (kz, kp, beta, t_list, target_value, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
    
 
-def follow_drift(seed_freq, target_value, param, increment, guess_fn, show_plot=False):
+def generate_vn_steps(lst, target_lst, lin_incrmt):
     """
-    to implement
-    """    
-    return 0
+    Generate search steps when following solution along v_list or n_list.
+    Gurantees linear change from 
+    """
+    start = np.array(lst)
+    end = np.array(target_lst)
+    num_step = 1 + np.amax(np.abs(start - end)/lin_incrmt)
+    num_step = int(round(num_step))
+    incrmt_list = (end - start)/num_step
+    res = np.array([start + incrmt_list * i  for i in range(num_step+1)])
+    return res
 
+def follow_drift(seed_freq, target_value, param, show_plot=False,
+                 lin_incrmt=0.1):
+    """
+    follow a mode with frequency SEED_FREQ in a plasma specified by
+    PARAM along the drift parameter.
+    
+    Caution: system with current are difficult to follow since dispersion
+    relation changes quickly.
+    We do not intend to provide assurance that solution converges.
+    """
+    (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+     v_list, n, method, aol) = param
+    seed_v_list = v_list
+    # a list of drift to step through
+    v_list_steps = generate_vn_steps(v_list, target_value,
+                                    lin_incrmt=lin_incrmt)
+    freq_lst = []
+    guess = seed_freq
+    for v_list in v_list_steps:
+        f = lambda wrel: real_imag(disp_det(
+            list_to_complex(wrel), kz, kp, beta, t_list, a_list,
+            n_list, q_list, m_list, v_list, n=n, method=method, aol=aol))
+        freq = scipy.optimize.fsolve(f, real_imag(guess))
+        guess = list_to_complex(freq)
+        freq_lst += [guess]
+    if show_plot:
+        plt.plot(np.abs(np.real(freq_lst)), 'o-', markersize= 2)
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.xlabel('step')
+        plt.ylabel(r'$\omega/\Omega_{ci}$')
+        plt.title(r'Change drift speed')
+        plt.show()        
+    new_param = (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
+def follow_density(seed_freq, target_value, param, show_plot=False,
+                   lin_incrmt=0.1):
+    """
+    follow a mode with frequency SEED_FREQ in a plasma specified by
+    PARAM along the drift parameter.
+    
+    Caution: system with current are difficult to follow since dispersion
+    relation changes quickly.
+    We do not intend to provide assurance that solution converges.
+    """
+    (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+     v_list, n, method, aol) = param
+    seed_n_list = n_list
+    # a list of relative density to step through
+    n_list_steps = generate_vn_steps(n_list, target_value,
+                                    lin_incrmt=lin_incrmt)
+    freq_lst = []
+    guess = seed_freq
+    for n_list in n_list_steps:
+        f = lambda wrel: real_imag(disp_det(
+            list_to_complex(wrel), kz, kp, beta, t_list, a_list,
+            n_list, q_list, m_list, v_list, n=n, method=method, aol=aol))
+        freq = scipy.optimize.fsolve(f, real_imag(guess))
+        guess = list_to_complex(freq)
+        freq_lst += [guess]
+    if show_plot:
+        plt.plot(np.abs(np.real(freq_lst)), 'o-', markersize= 2)
+        plt.yscale('log')
+        plt.xlabel('step')
+        plt.ylabel(r'$\omega/\Omega_{ci}$')
+        plt.title(r'Change density')
+        plt.show()        
+    new_param = (kz, kp, beta, t_list, a_list, n_list, q_list, m_list,
+                 v_list, n, method, aol)
+    return (guess, new_param, freq_lst)
 
--- unittest/test_follow.py
+++ unittest/test_follow.py
@@ -5,7 +5,7 @@ from py_vlasov.util import real_imag, kzkp
 from py_vlasov.follow_parameter import simple_follow_fn
 from py_vlasov.new_follow_parameter import (
     follow_kz, follow_kp, follow_k, follow_angle, follow_beta,
-    follow_temperature, follow_anisotropy, follow_drift)
+    follow_temperature, follow_anisotropy, follow_drift, follow_density)
 
 
 class Test_follow(unittest.TestCase):
@@ -37,12 +37,14 @@ class Test_follow(unittest.TestCase):
         log_inc = 0.2
         freq = follow_kz(seed_freq, target_kz, param,
                          log_incrmt=log_inc, incrmt_method = 'log')
+        freq = freq[0]
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 9.9115e-2, rtol = 1e-3)
         npt.assert_allclose(w_i, - 2.2500e-4, rtol = 1e-3)
 
         target_kz = 0.2
         freq = follow_kz(seed_freq, target_kz, param )
+        freq = freq[0]
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 1.8294e-1, rtol = 1e-3)
         npt.assert_allclose(w_i, - 4.6795e-3, rtol = 1e-3)        
@@ -73,12 +75,14 @@ class Test_follow(unittest.TestCase):
         
         target_kp = 0.5
         freq = follow_kp(seed_freq, target_kp, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 1.0333e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -1.0742e-4, rtol = 1e-4)
 
         target_kp = 1.0
         freq = follow_kp(seed_freq, target_kp, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 1.1366e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -3.2756e-4, rtol = 1e-4)
@@ -108,12 +112,14 @@ class Test_follow(unittest.TestCase):
                  m_list, v_list, n, method, aol]
         target_anisotropy = [2., 2.]
         freq = follow_anisotropy(seed_freq, target_anisotropy, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 1.4079e-1, rtol = 1e-4)
         npt.assert_allclose(w_i, -5.3245e-5, rtol = 1e-4)
 
         target_anisotropy = [.2, .2]
         freq = follow_anisotropy(seed_freq, target_anisotropy, param)
+        freq = freq[0]                
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 4.31826e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -4.458088e-5, rtol = 1e-4)        
@@ -145,6 +151,7 @@ class Test_follow(unittest.TestCase):
         target_beta = 0.1
         seed_freq = 0.06 - 0.04j        
         freq = follow_beta(seed_freq, target_beta, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 7.0589e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -3.35809e-2, rtol = 1e-4)
@@ -154,6 +161,7 @@ class Test_follow(unittest.TestCase):
         target_beta = 0.1
         seed_freq = 0.25        
         freq = follow_beta(seed_freq, target_beta, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r,0.3381558, rtol = 1e-4)
         npt.assert_allclose(w_i, -1.54426867e-3, rtol = 1e-4)        
@@ -164,6 +172,7 @@ class Test_follow(unittest.TestCase):
         target_beta = 0.1
         seed_freq = 0.1       
         freq = follow_beta(seed_freq, target_beta, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 0.1561297, rtol = 1e-4)
         npt.assert_allclose(w_i, -4.5627037e-5, rtol = 1e-4)
@@ -193,8 +202,9 @@ class Test_follow(unittest.TestCase):
         # Alfven mode
         
         target_temperature = [1., 10.]
-        seed_freq = 0.1
+        seed_freq = 0.05
         freq = follow_temperature(seed_freq, target_temperature, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r,4.997801e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -1.093311e-4, rtol = 1e-4)
@@ -202,8 +212,9 @@ class Test_follow(unittest.TestCase):
         # fast mode
         
         target_temperature = [1., 10.]
-        seed_freq = 0.3
+        seed_freq = 0.2
         freq = follow_temperature(seed_freq, target_temperature, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 0.257211, rtol = 1e-4)
         npt.assert_allclose(w_i, -4.280582e-3, rtol = 1e-4)
@@ -213,8 +224,188 @@ class Test_follow(unittest.TestCase):
         target_temperature = [1., 5.]
         seed_freq = 0.06 - 0.04j
         freq = follow_temperature(seed_freq, target_temperature, param)
+        freq = freq[0]        
         w_r, w_i = real_imag(freq)
         npt.assert_allclose(w_r, 7.1758e-2, rtol = 1e-4)
         npt.assert_allclose(w_i, -2.6035477e-2, rtol = 1e-4)
+
+    def test_follow_k_mhd(self):
+        """
+        test follow_k() on MHD modes.
+        Benchmarked against DSHARK.
+        """
+        kz = 0.05
+        kp = 0.086602540378443865 
+        
+        betap = 1.
+        t_list=[1.,1]
+        a_list=[1., 1.]
+        n_list=[1.,1.] 
+        q_list=[1.,-1.]
+        m_list=[1., 1./1836.]
+        v_list=[0.,0.]
+        n = 10
+        method = 'pade'
+        aol=1/5000
+
+        param = [kz, kp, betap, t_list, a_list, n_list, q_list,
+                 m_list, v_list, n, method, aol]
+
+        # Alfven mode
+        
+        target_k = 1.0
+        seed_freq = 0.05
+        freq = follow_k(seed_freq, target_k, param)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 0.4176781, rtol = 1e-4)
+        npt.assert_allclose(w_i, -0.1325794, rtol = 1e-4)
+
+        # fast mode
+        
+        target_k = 1.0
+        seed_freq = 0.2
+        freq = follow_k(seed_freq, target_k, param,
+                        log_incrmt = 0.05)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 1.563559, rtol = 1e-4)
+        npt.assert_allclose(w_i, -0.1767448, rtol = 1e-4)
+
+        # slow mode
+        
+        target_k = 1.0
+        seed_freq = 0.06 - 0.04j
+        freq = follow_k(seed_freq, target_k, param)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 0.5930758, rtol = 1e-4)
+        npt.assert_allclose(w_i, -0.3922563, rtol = 1e-4)        
+
+    def test_follow_k_mirror(self):
+        """
+        test follow_k() on mirror modes.
+        Benchmarked against DSHARK.
+        """
+        k = 0.1
+        angle = 71. * np.pi/180.
+        kz = k * np.cos(angle)
+        kp = k * np.sin(angle)
+        art_vd = 0.5
+        betap = 1.
+        t_list=[1.,1]
+        a_list=[2., 1.]
+        n_list=[1.,1.] 
+        q_list=[1.,-1.]
+        m_list=[1., 1./1836.]
+        v_list=[art_vd, art_vd]
+        n = 10
+        method = 'pade'
+        aol=1/5000
+
+        param = [kz, kp, betap, t_list, a_list, n_list, q_list,
+                 m_list, v_list, n, method, aol]
+
+        # Alfven mode
+        
+        target_k = 0.5
+        doppler_shift = kz * art_vd/np.sqrt(betap)
+        seed_freq = doppler_shift + 3e-3j 
+        freq = follow_k(seed_freq, target_k, param)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        doppler_shift = target_k * np.cos(angle) * art_vd/np.sqrt(betap)
+        npt.assert_allclose(w_r, doppler_shift, rtol = 1e-8)
+        npt.assert_allclose(w_i, 0.5739430E-02, rtol = 1e-4)
+
+    def test_follow_drift_alfven_fast(self):
+        """
+        test follow_tempature() on Alfven and fast mode.
+        Benchmarked against WHAMP (which has difficulty finding slow mode).
+        """
+        kz = 0.05
+        kp = 0.086602540378443865 
+        
+        betap = 1.
+        t_list=[1.,1., 1.]
+        a_list=[1., 1., 1.]
+        n_list=[1.,.9, .1] 
+        q_list=[1.,-1., -1.]
+        m_list=[1., 1./1836., 1./1836.]
+        v_list=[0., 0., 0.]
+        n = 10
+        method = 'pade'
+        aol=1/5000
+
+        param = [kz, kp, betap, t_list, a_list, n_list, q_list,
+                 m_list, v_list, n, method, aol]
+
+        # Alfven mode
+        
+        target_drift = [0., 1., -9.]
+        seed_freq = 0.05
+        freq = follow_drift(seed_freq, target_drift, param,lin_incrmt=0.5)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r,4.9827e-2, rtol = 1e-4)
+        npt.assert_allclose(w_i, -1.8566e-5, rtol = 1e-4)
+
+        # fast mode
+        
+        target_drift = [0., 1., -9.]
+        seed_freq = 0.15
+        freq = follow_drift(seed_freq, target_drift, param,lin_incrmt=0.5)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 1.5034e-1, rtol = 1e-4)
+        npt.assert_allclose(w_i,- 1.4559e-3, rtol = 1e-4)
+
+    def test_follow_density_mhd(self):
+        """
+        test follow_density() on MHD modes.
+        Benchmarked against DSHARK.
+        """
+        kz = 0.05
+        kp = 0.086602540378443865 
+        
+        betap = 1.
+        t_list=[1., 1., 10.]
+        a_list=[1., 1., 1.]
+        n_list=[1., 1., 0.] 
+        q_list=[1.,-1., -1.]
+        m_list=[1., 1./1836., 1./1836.]
+        v_list=[0., 0., 0.]
+        n = 10
+        method = 'pade'
+        aol=1/5000
+
+        param = [kz, kp, betap, t_list, a_list, n_list, q_list,
+                 m_list, v_list, n, method, aol]
+
+        # Alfven mode
+        
+        target_density = [1., 0., 1.]
+        seed_freq = 0.05
+        freq = follow_density(seed_freq, target_density, param,lin_incrmt=0.1)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 4.997801138e-002, rtol = 1e-4)
+        npt.assert_allclose(w_i, -1.0933112E-004, rtol = 1e-4)
+
+        # Fast mode
+        seed_freq = 0.15
+        freq = follow_density(seed_freq, target_density, param,lin_incrmt=0.05)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 0.2572110929, rtol = 1e-4)
+        npt.assert_allclose(w_i, -4.2805821078e-3, rtol = 1e-4)        
+
+        # slow mode
+        seed_freq = 0.06 - 0.04j
+        freq = follow_density(seed_freq, target_density, param,lin_incrmt=0.05)
+        freq = freq[0]        
+        w_r, w_i = real_imag(freq)
+        npt.assert_allclose(w_r, 7.46495745e-2, rtol = 1e-4)
+        npt.assert_allclose(w_i, -2.342562429e-2, rtol = 1e-4)        
 if __name__ == '__main__':
     unittest.main()
